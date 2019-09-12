23adbca35520af848f9bd80eab3a3034c68717a4
devwalker <vatik.vag@gmail.com>
2019-09-13 01:19:19 +0300

    add video path to application.properties

--- src/main/java/com/example/videos/rest/HelloWorldController.java
+++ src/main/java/com/example/videos/rest/HelloWorldController.java
@@ -1,14 +1,17 @@
 package com.example.videos.rest;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 public class HelloWorldController {
 
     @RequestMapping({ "/hello" })
-    public String firstPage() {
-        return "Hello World";
+    public String firstPage(Principal principal) {
+        return principal.getName();
     }
 
 }
