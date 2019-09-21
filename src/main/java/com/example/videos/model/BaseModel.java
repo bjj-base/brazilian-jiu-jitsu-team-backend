1ffab8844bd06d58158546809995310de54a3356
devwalker <vatik.vag@gmail.com>
2019-09-21 21:46:45 +0300

    @CreatedAt and @UpdatedAt

--- src/main/java/com/example/videos/model/BaseModel.java
+++ src/main/java/com/example/videos/model/BaseModel.java
@@ -1,16 +1,22 @@
 package com.example.videos.model;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Data
+@EntityListeners(AuditingEntityListener.class)
 public class BaseModel {
+
+    public BaseModel() {}
 
     @Id
     @Type(type = "java.lang.Long")
@@ -18,11 +24,12 @@ public class BaseModel {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "SEQ_STORE")
     protected Long id;
 
-    @CreatedDate
+
+    @CreationTimestamp
     @Column(name = "inserted_at",  insertable = true, updatable = true)
     protected LocalDateTime insertedAt;
 
-    @LastModifiedDate
+    @UpdateTimestamp
     @Column(name = "updated_at",  insertable = true, updatable = true)
     protected LocalDateTime updatedAt;
 }
